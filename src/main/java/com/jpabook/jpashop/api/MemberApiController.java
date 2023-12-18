5a5901b13012ff370308757158d6df0e05277ae3
정우혁 <alalalalalalall367@gmail.com>
2023-12-18 23:05:17 +0900

    feat : 회원수정 api 개발

--- src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -6,9 +6,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,6 +31,16 @@ public class MemberApiController {
         return new CreateMemberResponse(join);
     }
 
+    @PutMapping("api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2
+            (@PathVariable("id") Long id,
+             @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
     @Data
     static class CreateMemberRequest {
         private String name;
@@ -42,6 +50,18 @@ public class MemberApiController {
     @AllArgsConstructor
     static class CreateMemberResponse {
         private Long id;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 
 }
