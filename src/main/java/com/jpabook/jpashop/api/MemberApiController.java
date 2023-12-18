bbc376b4d1688dc13feffefd8ead129f82a0a3be
정우혁 <alalalalalalall367@gmail.com>
2023-12-18 22:38:27 +0900

    feat : 회원등록 api 개발

---
+++ src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,47 @@
+package com.jpabook.jpashop.api;
+
+import com.jpabook.jpashop.domain.Member;
+import com.jpabook.jpashop.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.name);
+
+        Long join = memberService.join(member);
+
+        return new CreateMemberResponse(join);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class CreateMemberResponse {
+        private Long id;
+    }
+
+}
